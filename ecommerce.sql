8378f59d78b1e9684fef5fa11838441d35cd1899
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-15 11:22:28 -0500

    (134)  Brand Page Design Part 2

--- ecommerce.sql
+++ ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 03, 2021 at 06:45 PM
+-- Generation Time: Nov 14, 2021 at 06:26 PM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.3.21
 
@@ -48,7 +48,26 @@ CREATE TABLE IF NOT EXISTS `admins` (
 --
 
 INSERT INTO `admins` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
-(1, 'Admin', 'admin@gmail.com', '2021-10-31 20:50:07', '$2y$10$kMtiu6Le24Untbh/uHRz.ebpe2h.Bpx38z/C3Yo6CMyucjyvqYktm', '02peciyi13puVmU06Uxskhao8vvnWrLCWBx6UekYceYeID56JR7NI4Tma62S', NULL, '202111022032me.png', '2021-10-31 20:50:07', '2021-11-03 16:22:35');
+(1, 'Admin', 'admin@gmail.com', '2021-10-31 20:50:07', '$2y$10$kMtiu6Le24Untbh/uHRz.ebpe2h.Bpx38z/C3Yo6CMyucjyvqYktm', 'sRlZ9dTAJ7fnjnrg8a7NVuRbMv0XxIwWyrMqA04a547Kf9PM30Z5tS91eSUF', NULL, '202111022032me.png', '2021-10-31 20:50:07', '2021-11-03 16:22:35');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brands`
+--
+
+DROP TABLE IF EXISTS `brands`;
+CREATE TABLE IF NOT EXISTS `brands` (
+  `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `brand_name_en` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_name_hin` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_slug_en` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_slug_hin` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brand_image` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -81,7 +100,7 @@ CREATE TABLE IF NOT EXISTS `migrations` (
   `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `batch` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dumping data for table `migrations`
@@ -94,7 +113,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (4, '2019_08_19_000000_create_failed_jobs_table', 1),
 (5, '2019_12_14_000001_create_personal_access_tokens_table', 2),
 (6, '2021_10_28_222439_create_sessions_table', 2),
-(7, '2021_10_31_162337_create_admins_table', 3);
+(7, '2021_10_31_162337_create_admins_table', 3),
+(8, '2021_11_14_172138_create_brands_table', 4);
 
 -- --------------------------------------------------------
 
@@ -156,8 +176,9 @@ CREATE TABLE IF NOT EXISTS `sessions` (
 --
 
 INSERT INTO `sessions` (`id`, `user_id`, `ip_address`, `user_agent`, `payload`, `last_activity`) VALUES
-('XO3kZly61GcogPWy3NGGKTNUCgJGofDmUKlfQNrr', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiOGR3d2dBY0t0d01KdGRzTVI0SklXcDd4SmVnS0x1cFVEbDlQYmZDWiI7czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MjE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMCI7fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7czoxNzoicGFzc3dvcmRfaGFzaF93ZWIiO3M6NjA6IiQyeSQxMCRwTHFKYm0zU01YNmVUL2dtUkwvcXYuMGx4MFJoNi8waWxzRWhwbDYvUng2bFlDZ3ZsRXdVNiI7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkcExxSmJtM1NNWDZlVC9nbVJML3F2LjBseDBSaDYvMGlsc0VocGw2L1J4NmxZQ2d2bEV3VTYiO30=', 1635964778),
-('tCwUETKxUlyBbtpOeeBnMyW9gtr0uOqE93KlKOKa', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoidHJZdFdxRUFpbGNlVzRNcEJhbEJJNE43eG9QUTBub2JPelJieVdycyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6Mjc6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9sb2dpbiI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTI6ImxvZ2luX2FkbWluXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MTt9', 1635954092);
+('dEpUnAMCNUJihaHC0kok0TDF66wNEj84YxF1tTO7', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36', 'YTo3OntzOjM6InVybCI7YTowOnt9czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzE6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9kYXNoYm9hcmQiO31zOjY6Il9mbGFzaCI7YToyOntzOjM6Im9sZCI7YTowOnt9czozOiJuZXciO2E6MDp7fX1zOjY6Il90b2tlbiI7czo0MDoiZDUwOHBnbTh0cXhNQ0JzdDZjTVhnUzF6VFpIQjBvQmZCQjRQcWh4eCI7czo1MDoibG9naW5fd2ViXzU5YmEzNmFkZGMyYjJmOTQwMTU4MGYwMTRjN2Y1OGVhNGUzMDk4OWQiO2k6MjtzOjE3OiJwYXNzd29yZF9oYXNoX3dlYiI7czo2MDoiJDJ5JDEwJFRTTHI1LnNIQzZEVVRvUS8vaHNTYXVOS3FXMXNMYWxFU0ZTV05GU1RyLzlFaHFrT3ZiR0dHIjtzOjIxOiJwYXNzd29yZF9oYXNoX3NhbmN0dW0iO3M6NjA6IiQyeSQxMCRUU0xyNS5zSEM2RFVUb1EvL2hzU2F1TktxVzFzTGFsRVNGU1dORlNUci85RWhxa092YkdHRyI7fQ==', 1636825618),
+('d1JH9A9Y8N57kf0DvBMRxg56nW3AdIfH98n14M5O', 2, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36', 'YTo2OntzOjY6Il90b2tlbiI7czo0MDoiYmYyQ2YybkdFVm16eGI4Qk5ITDhDSzQ3Z1BqVHU3dGtrZlduVnFXVyI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6NDI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC91c2VyL2NoYW5nZS9wYXNzd29yZCI7fXM6NjoiX2ZsYXNoIjthOjI6e3M6Mzoib2xkIjthOjA6e31zOjM6Im5ldyI7YTowOnt9fXM6NTA6ImxvZ2luX3dlYl81OWJhMzZhZGRjMmIyZjk0MDE1ODBmMDE0YzdmNThlYTRlMzA5ODlkIjtpOjI7czoxNzoicGFzc3dvcmRfaGFzaF93ZWIiO3M6NjA6IiQyeSQxMCRUU0xyNS5zSEM2RFVUb1EvL2hzU2F1TktxVzFzTGFsRVNGU1dORlNUci85RWhxa092YkdHRyI7czoyMToicGFzc3dvcmRfaGFzaF9zYW5jdHVtIjtzOjYwOiIkMnkkMTAkVFNMcjUuc0hDNkRVVG9RLy9oc1NhdU5LcVcxc0xhbEVTRlNXTkZTVHIvOUVocWtPdmJHR0ciO30=', 1636898320),
+('FR0stoc5BLVxsh1IPembYnMTUbOWr8qgG3oEncTs', NULL, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36', 'YTo0OntzOjY6Il90b2tlbiI7czo0MDoiY242NjBjbjN4c3pQQ1F5bGg2a01CcmR1U3RlaDkwUnRIeVJmdm1GNiI7czo5OiJfcHJldmlvdXMiO2E6MTp7czozOiJ1cmwiO3M6MzI6Imh0dHA6Ly8xMjcuMC4wLjE6ODAwMC9icmFuZC92aWV3Ijt9czo2OiJfZmxhc2giO2E6Mjp7czozOiJvbGQiO2E6MDp7fXM6MzoibmV3IjthOjA6e319czo1MjoibG9naW5fYWRtaW5fNTliYTM2YWRkYzJiMmY5NDAxNTgwZjAxNGM3ZjU4ZWE0ZTMwOTg5ZCI7aToxO30=', 1636914101);
 
 -- --------------------------------------------------------
 
@@ -190,7 +211,7 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `name`, `email`, `phone`, `email_verified_at`, `password`, `two_factor_secret`, `two_factor_recovery_codes`, `remember_token`, `current_team_id`, `profile_photo_path`, `created_at`, `updated_at`) VALUES
 (1, 'User', 'user@gmail.com', NULL, NULL, '$2y$10$PkN00Yc4LdZJkMxraq4Wo.Ao/Vhd5sD68KroaHMpc5pvtHbR6i4UW', NULL, NULL, NULL, NULL, 'profile-photos/92ra8pMNQBSCqUZUZe4ve3hhQ5mxEb7kfynecQz6.png', '2021-10-29 02:50:49', '2021-10-29 03:12:52'),
-(2, 'Myron', 'myronrogers.rogers@gmail.com', '9048918679', NULL, '$2y$10$pLqJbm3SMX6eT/gmRL/qv.0lx0Rh6/0ilsEhpl6/Rx6lYCgvlEwU6', NULL, NULL, NULL, NULL, NULL, '2021-11-03 22:14:43', '2021-11-03 22:14:43');
+(2, 'Myron', 'myronrogers.rogers@gmail.com', '9048918679', NULL, '$2y$10$TSLr5.sHC6DUToQ//hsSauNKqW1sLalESFSWNFSTr/9EhqkOvbGGG', NULL, NULL, 'FQq3mzB0qTMLF4RP5zSEI2kllqwe0HyfU0jz1K213hLf5CceD2PbGZeHpkOE', NULL, '202111042041sylvester.png', '2021-11-03 22:14:43', '2021-11-13 22:38:12');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\Backend\AdminProfileController;
+use App\Http\Controllers\Backend\BrandController;
 use App\Http\Controllers\Frontend\IndexController;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -63,3 +64,10 @@ Route::post('/user/profile/store', [IndexController::class, 'UserProfileStore'])
 Route::get('/user/change/password', [IndexController::class, 'UserChangePassword'])->name('change.password'); 
 
 Route::post('/user/password/update', [IndexController::class, 'UserPasswordUpdate'])->name('user.password.update'); 
+
+
+// Admin Brand All Routes
+
+Route::prefix('brand')->group(function(){
+    Route::get('/view', [BrandController::class, 'BrandView'])->name('all.brand'); 
+});
