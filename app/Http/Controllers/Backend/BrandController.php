8d294fb3ee9e9dd9b82c2a664cf2223058c199d3
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-16 15:32:58 -0500

    (140) Brand Crud Part 3 Delete with SweetAlert Alert 2

--- app/Http/Controllers/Backend/BrandController.php
+++ app/Http/Controllers/Backend/BrandController.php
@@ -93,6 +93,19 @@ class BrandController extends Controller
 
 
     }
+
+    public function BrandDelete($id){
+        $brand = Brand::findOrFail($id);
+        $img = $brand->brand_image;
+        unlink($img);
+
+        Brand::findOrFail($id)->delete();
+        $notification = array(
+            'message' => 'Brand Deleted Successfully',
+            'alert-type' => 'info',
+        );
+        return redirect()->back()->with($notification);
+    }
 }
 
 
--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -64,7 +64,7 @@
     <script src="{{ asset('backend/js/template.js') }}"></script>
     <script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js" integrity="sha512-VEd+nq25CkR676O+pLBnDW09R7VQX9Mdiij052gVCp5yVH3jGtH70Ho/UUv4mJDsEdTvqRCFZg0NKGiojGnUCw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>     
- 
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script>
  
    @if(Session::has('message')) {
@@ -93,7 +93,36 @@
   }
   @endif
   </script>  
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script type="text/javascript">
+$(function(){
+  $(document).on('click', '#delete',function(e){
+    e.preventDefault();
+    var link = $(this).attr("href");
 
+Swal.fire({
+  title: 'Are you sure?',
+  text: "Delete this Data?",
+  icon: 'warning',
+  showCancelButton: true,
+  confirmButtonColor: '#3085d6',
+  cancelButtonColor: '#d33',
+  confirmButtonText: 'Yes, delete it!'
+}).then((result) => {
+  if (result.isConfirmed) {
+    window.location.href = link
+    Swal.fire(
+      'Deleted!',
+      'Your file has been deleted.',
+      'success'
+    )
+  }
+})
+
+});
+});
+
+</script>
 </body>
 
 </html>
