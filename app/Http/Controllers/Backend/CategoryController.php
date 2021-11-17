b82b2d0e53e2339dc4fe512be10c4e5af4c1f794
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-17 11:39:58 -0500

    (143) Category Crud Part 3 and (144) Category Crud Part 4

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -38,4 +38,40 @@ class CategoryController extends Controller
         );
         return redirect()->back()->with($notification);
     }
+
+    public function CategoryEdit($id) {
+        $category = Category::findOrFail($id);
+        return view('backend.category.category_edit',compact('category'));
+    }
+
+    public function CategoryUpdate(Request $request) {
+        $category_id = $request->id;
+        $old_icon = $request->old_icon;
+
+             Category::findOrFail($category_id)->update([
+                'category_name_en'  => $request->category_name_en,
+                'category_name_hin'  => $request->category_name_hin,
+                'category_slug_en'  => strtolower(str_replace(' ','-' ,$request->category_name_en)),
+                'category_slug_hin'  => str_replace(' ','-' ,$request->category_name_hin),
+                'category_icon'  => $request->category_icon,
+    
+            ]);
+            $notification = array(
+                'message' => 'Category Updated Successfully',
+                'alert-type' => 'info'
+            );
+            return redirect()->route('all.category')->with($notification);
+    }
+
+    public function CategoryDelete($id){
+        $category = Category::findOrFail($id);
+        Category::findOrFail($id)->delete();
+        $notification = array(
+            'message' => 'Category Deleted Successfully',
+            'alert-type' => 'info',
+        );
+        return redirect()->back()->with($notification);
+    }
+
+
 }
--- resources/views/backend/category/category_edit.blade.php
+++ resources/views/backend/category/category_edit.blade.php
@@ -6,65 +6,24 @@
         <!-- Main content -->
         <section class="content">
             <div class="row">
-
-                <div class="col-8">
+                <!-- -------------- Edit Category Page ---------------- -->
+                <div class="col-12">
 
                     <div class="box">
                         <div class="box-header with-border">
-                            <h3 class="box-title">Category List</h3>
+                            <h3 class="box-title">Edit Category</h3>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
                             <div class="table-responsive">
-                                <table id="example1" class="table table-bordered table-striped">
-                                    <thead>
-
-                                        <tr>
-                                            <th>Category Icon</th>
-                                            <th>Category En</th>
-                                            <th>Category Hin</th>                                           
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($category as $item)
-                                            <tr>
-                                                <td><span><i class="{{ $item->category_icon }}"></i></span></td>
-                                                <td>{{ $item->category_name_en }}</td>
-                                                <td>{{ $item->category_name_hin }}</td>
-                                                  <td>  <a href="{{ route('category.edit', $item->id) }}" class="btn btn-info" title"Edit Data"><i class="fa fa-pencil"></i></a>
-                                                    <a href="{{ route('category.delete', $item->id) }}" class="btn btn-info" id="delete" title"Delete Data"><i class="fa fa-trash"></i></a>
-                                                                                                    </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <!-- /.box-body -->
-                    </div>
-                    <!-- /.box -->
-
-
-                </div>
-                <!-- /.col -->
-                <!-- -------------- Add Category Page ---------------- -->
-                <div class="col-4">
-
-                    <div class="box">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Add Category</h3>
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                            <div class="table-responsive">
-                                <form method="post" action="{{ route('category.store') }}">
+                                <form method="post" action="{{ route('category.update', $category->id) }}">
                                     @csrf
+                                    <input type="hidden" name="id" id="id" value="{{ $category->id }}">
                                                 <div class="form-group">
                                                     <h5>Category English <span class="text-danger">*</span></h5>
                                                     <div class="controls">
                                                         <input type="text" id="category_name_en" name="category_name_en"
-                                                            class="form-control">
+                                                            class="form-control" value="{{ $category->category_name_en }}">
                                                             @error('category_name_en')
                                                             <span class="text-danger">{{ $message }}</span>
                                                             @enderror
@@ -75,7 +34,7 @@
                                                     <h5>Category Hindi <span class="text-danger">*</span></h5>
                                                     <div class="controls">
                                                         <input type="text" id="category_name_hin" name="category_name_hin"
-                                                            class="form-control">
+                                                            class="form-control" value="{{ $category->category_name_hin }}">
                                                             @error('category_name_hin')
                                                             <span class="text-danger">{{ $message }}</span>
                                                             @enderror
@@ -87,7 +46,7 @@
                                                     <h5>Category Icon <span class="text-danger">*</span></h5>
                                                     <div class="controls">
                                                         <input type="text" id="category_icon" name="category_icon"
-                                                            class="form-control">
+                                                            class="form-control" value="{{ $category->category_icon }}">
                                                             @error('category_icon')
                                                             <span class="text-danger">{{ $message }}</span>
                                                             @enderror
@@ -97,7 +56,7 @@
 
 
                                     <div class="text-xs-right">
-                                        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add Category">
+                                        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
                                     </div>
                                 </form>
 
