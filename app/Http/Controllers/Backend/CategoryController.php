62d4f513c5016e161c9ee8187e8d4c4e71c7a11c
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-17 10:54:28 -0500

    (142) Category Crud Part 2

--- app/Http/Controllers/Backend/CategoryController.php
+++ app/Http/Controllers/Backend/CategoryController.php
@@ -12,4 +12,30 @@ class CategoryController extends Controller
         $category = Category::latest()->get();
         return view('backend.category.category_view',compact('category'));
     }
+
+    public function CategoryStore(Request $request) {
+        $request->validate([
+            'category_name_en' => 'required',
+            'category_name_hin' => 'required',
+            'category_icon' => 'required',    
+        ],[
+            'category_name_en.required' => 'Input Category English Name',
+            'category_name_hin.required' => 'Input Category Hindi Name',
+
+        ]);
+
+        Category::insert([
+            'category_name_en'  => $request->category_name_en,
+            'category_name_hin'  => $request->category_name_hin,
+            'category_slug_en'  => strtolower(str_replace(' ','-' ,$request->category_name_en)),
+            'category_slug_hin'  => str_replace(' ','-' ,$request->category_name_hin),
+            'category_icon'  => $request->category_icon,
+
+        ]);
+        $notification = array(
+            'message' => 'Category Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
 }
