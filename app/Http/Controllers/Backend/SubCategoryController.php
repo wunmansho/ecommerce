76891316a4676376ae19b38ad2fc484ba3d7f0e8
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-17 14:22:02 -0500

    (146) Subcategory Crud Part 2

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -5,25 +5,30 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\SubCategory;
+use App\Models\Category;
 
 class SubCategoryController extends Controller
 {
     public function SubCategoryView(){
+        $categories = Category::orderBy('category_name_en', 'ASC')->get();
         $subcategory = SubCategory::latest()->get();
-        return view('backend.category.subcategory_view',compact('subcategory'));
+        return view('backend.category.subcategory_view',compact('subcategory','categories'));
     }
 
     public function SubCategoryStore(Request $request) {
         $request->validate([
+            'category_id' => 'required',
             'subcategory_name_en' => 'required',
             'subcategory_name_hin' => 'required',
         ],[
+            'category_id.required' => 'Please Select An option',
             'subcategory_name_en.required' => 'Input SubCategory English Name',
             'subcategory_name_hin.required' => 'Input SubCategory Hindi Name',
 
         ]);
 
         SubCategory::insert([
+            'category_id'  => $request->category_id,
             'subcategory_name_en'  => $request->subcategory_name_en,
             'subcategory_name_hin'  => $request->subcategory_name_hin,
             'subcategory_slug_en'  => strtolower(str_replace(' ','-' ,$request->subcategory_name_en)),
@@ -38,8 +43,9 @@ class SubCategoryController extends Controller
     }
 
     public function SubCategoryEdit($id) {
+        $categories = Category::orderBy('category_name_en', 'ASC')->get();
         $subcategory = SubCategory::findOrFail($id);
-        return view('backend.category.subcategory_edit',compact('subcategory'));
+        return view('backend.category.subcategory_edit',compact('subcategory', 'categories'));
     }
 
     public function SubCategoryUpdate(Request $request) {
--- resources/views/backend/category/subcategory_edit.blade.php
+++ resources/views/backend/category/subcategory_edit.blade.php
@@ -7,53 +7,12 @@
         <section class="content">
             <div class="row">
 
-                <div class="col-8">
+                 <!-- -------------- Edit SubCategory Page ---------------- -->
+                <div class="col-12">
 
                     <div class="box">
                         <div class="box-header with-border">
-                            <h3 class="box-title">SubCategory List</h3>
-                        </div>
-                        <!-- /.box-header -->
-                        <div class="box-body">
-                            <div class="table-responsive">
-                                <table id="example1" class="table table-bordered table-striped">
-                                    <thead>
-
-                                        <tr>
-                                            <th>Category </th>
-                                            <th>SubCategory En</th>
-                                            <th>SubCategory Hin</th>                                           
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($subcategory as $item)
-                                            <tr>
-                                                <td>{{ $item->category_id }}</td>
-                                                <td>{{ $item->subcategory_name_en }}</td>
-                                                <td>{{ $item->subcategory_name_hin }}</td>
-                                                  <td>  <a href="{{ route('subcategory.edit', $item->id) }}" class="btn btn-info" title"Edit Data"><i class="fa fa-pencil"></i></a>
-                                                    <a href="{{ route('subcategory.delete', $item->id) }}" class="btn btn-info" id="delete" title"Delete Data"><i class="fa fa-trash"></i></a>
-                                                                                                    </td>
-                                            </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                        <!-- /.box-body -->
-                    </div>
-                    <!-- /.box -->
-
-
-                </div>
-                <!-- /.col -->
-                <!-- -------------- Add SubCategory Page ---------------- -->
-                <div class="col-4">
-
-                    <div class="box">
-                        <div class="box-header with-border">
-                            <h3 class="box-title">Add SubCategory</h3>
+                            <h3 class="box-title">Edit SubCategory</h3>
                         </div>
                         <!-- /.box-header -->
                         <div class="box-body">
@@ -66,7 +25,7 @@
                                                         <select name="category_id" id="category_id" class="form-control">
                                                             <option value="" selected="" disabled="">Select Category</option>
                                                             @foreach($categories as $category)
-                                                            <option value="{{ $category->id }}">{{ $category->category_name_en }}</option>
+                                                            <option value="{{ $category->id }}" {{ $category->id == $subcategory->category_id ? 'selected': '' }}>{{ $category->category_name_en }}</option>
                                                             @endforeach
                                                           </select>
                                                           @error('category_id')
@@ -79,7 +38,7 @@
                                                     <h5>SubCategory English <span class="text-danger">*</span></h5>
                                                     <div class="controls">
                                                         <input type="text" id="subcategory_name_en" name="subcategory_name_en"
-                                                            class="form-control">
+                                                            class="form-control" value="{{ $subcategory->subcategory_name_en }}">
                                                             @error('subcategory_name_en')
                                                             <span class="text-danger">{{ $message }}</span>
                                                             @enderror
@@ -91,7 +50,7 @@
                                                     <h5>SubCategory Hindi <span class="text-danger">*</span></h5>
                                                     <div class="controls">
                                                         <input type="text" id="subcategory_name_hin" name="subcategory_name_hin"
-                                                            class="form-control">
+                                                            class="form-control" value="{{ $subcategory->subcategory_name_hin }}">
                                                             @error('subcategory_name_hin')
                                                             <span class="text-danger">{{ $message }}</span>
                                                             @enderror
@@ -102,7 +61,7 @@
  
 
                                     <div class="text-xs-right">
-                                        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Add">
+                                        <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Update">
                                     </div>
                                 </form>
 
@@ -123,3 +82,5 @@
 
 
 @endsection
+
+
