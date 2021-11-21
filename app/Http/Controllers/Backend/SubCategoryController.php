581694303404d50e74478dc94a9d106442b18fb5
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-21 11:03:26 -0500

    (154) Add Product Database and Page Design Part 2

--- app/Http/Controllers/Backend/SubCategoryController.php
+++ app/Http/Controllers/Backend/SubCategoryController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\SubCategory;
 use App\Models\Category;
+use App\Models\SubCategory;
 use App\Models\SubSubCategory;
 
 class SubCategoryController extends Controller
