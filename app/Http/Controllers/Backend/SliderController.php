81ab433f72af9f0bb76bd5a19c1bd904dc1df1a7
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-11-23 21:05:21 -0500

    (173) Upload Slider and Show All Slider Part 2

--- app/Http/Controllers/Backend/SliderController.php
+++ app/Http/Controllers/Backend/SliderController.php
@@ -14,4 +14,28 @@ class SliderController extends Controller
         $sliders = Slider::latest()->get();
         return view('backend.slider.slider_view',compact('sliders'));
     }
+
+    public function SliderStore(Request $request) {
+        $request->validate([
+            'slider_img' => 'required',
+           ],[
+            'slider_img.required' => 'Please Select Image',
+         ]);
+        $image = $request->file('slider_img');
+        $name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+        Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+        $save_url = 'upload/slider/'.$name_gen;
+        Slider::insert([
+            'title'  => $request->title,
+            'description'  => $request->description,
+            'slider_img'  => $save_url,
+
+        ]);
+        $notification = array(
+            'message' => 'Slider Inserted Successfully',
+            'alert-type' => 'success'
+        );
+        return redirect()->back()->with($notification);
+    }
+
 }
