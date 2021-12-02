14ca76120084b23701f43a35614e74a1d92641e4
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-12-01 22:05:48 -0500

    (202) Tag Wise Product Show Part 4

--- resources/views/frontend/tags/tags_view.blade.php
+++ resources/views/frontend/tags/tags_view.blade.php
@@ -259,7 +259,7 @@ Tag Wise Product
                       <div class="products">
                         <div class="product">
                           <div class="product-image">
-                            <div class="image"> <a href="detail.html"><img  src="{{ asset($product->product_thumbnail) }}" alt=""></a> </div>
+                            <div class="image"> <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en) }}"><img  src="{{ asset($product->product_thumbnail) }}" alt=""></a> </div>
                             <!-- /.image -->
                             @php
                               $amount = $product->selling_price - $product->discount_price;
@@ -334,31 +334,36 @@ Tag Wise Product
                 <div class="category-product">
 
 
-                    
+                  @foreach($products as $product)   
                   <div class="category-product-inner wow fadeInUp">
                     <div class="products">
                       <div class="product-list product">
                         <div class="row product-list-row">
                           <div class="col col-sm-4 col-lg-4">
                             <div class="product-image">
-                              <div class="image"> <img src="{{ asset('frontend/assets/images/products/p3.jpg') }}" alt=""> </div>
+                              <div class="image"> <img src="{{ asset($product->product_thumbnail) }}" alt=""> </div>
                             </div>
                             <!-- /.product-image --> 
                           </div>
                           <!-- /.col -->
                           <div class="col col-sm-8 col-lg-8">
                             <div class="product-info">
-                              <h3 class="name"><a href="detail.html">Floral Print Buttoned</a></h3>
+                              <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en) }}">@if (session()->get('language') == 'hindi') {{ $product->product_name_hin }} @else {{ $product->product_name_en }} @endif</a></h3>
                               <div class="rating rateit-small"></div>
-                              <div class="product-price"> <span class="price"> $450.99 </span> <span class="price-before-discount">$ 800</span> </div>
-                              <!-- /.product-price -->
-                              <div class="description m-t-10">Suspendisse posuere arcu diam, id accumsan eros pharetra ac. Nulla enim risus, facilisis bibendum gravida eget, lacinia id purus. Suspendisse posuere arcu diam, id accumsan eros pharetra ac. Nulla enim risus, facilisis bibendum gravida eget.</div>
+                              @if ($product->discount_price == NULL) 
+                              <div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span></div>
+                              <!-- /.product-price -->  
+                              @else
+                              <div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">${{ $product->selling_price }}</span> </div>
+                              <!-- /.product-price --> 
+                              @endif  
+                              <div class="description m-t-10">@if (session()->get('language') == 'hindi') {{ $product->short_desc_hin }} @else {{ $product->short_desc_en }} @endif</div>
                               <div class="cart clearfix animate-effect">
                                 <div class="action">
                                   <ul class="list-unstyled">
                                     <li class="add-cart-button btn-group">
                                       <button class="btn btn-primary icon" data-toggle="dropdown" type="button"> <i class="fa fa-shopping-cart"></i> </button>
-                                      <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+                                      <button class="btn btn-primary cart-btn" type="button">@if (session()->get('language') == 'hindi') कार्ट में जोड़ें @else Add to cart @endif</button>
                                     </li>
                                     <li class="lnk wishlist"> <a class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
                                     <li class="lnk"> <a class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal"></i> </a> </li>
@@ -374,14 +379,25 @@ Tag Wise Product
                           <!-- /.col --> 
                         </div>
                         <!-- /.product-list-row -->
+                        @php
+                        $amount = $product->selling_price - $product->discount_price;
+                        $discount = ($amount/$product->selling_price) * 100;
+                      @endphp
+
+                      <div>
+                        @if ($product->discount_price == NULL) 
                         <div class="tag new"><span>new</span></div>
+                        @else
+                        <div class="tag hot"><span>{{ round($discount) }}%</span></div>
+                        @endif
+                      </div>                            
                       </div>
                       <!-- /.product-list --> 
                     </div>
                     <!-- /.products --> 
                   </div>
                   <!-- /.category-product-inner -->
-                  
+                  @endforeach
                   
                 </div>
                 <!-- /.category-product --> 
