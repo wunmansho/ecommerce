c74d417677d3e083ba361a5c454c49c3eba32168
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-12-06 13:12:14 -0500

    (209) Product Modal Design Part 1  ADD TO Cart

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -303,51 +303,52 @@ My Online Shop
                           </div>
                           <!-- /.product-image -->
                           
-                          <div class="product-info text-left">
-                            <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en) }}">@if (session()->get('language') == 'hindi') {{ $product->product_name_hin }} @else {{ $product->product_name_en }} @endif</a></h3>
-                            <div class="rating rateit-small"></div>
-                            <div class="description"></div>
-                            @if ($product->discount_price == NULL) 
-                            <div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span></div>
-                            <!-- /.product-price --> 
+      <div class="product-info text-left">
+        <h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en) }}">@if (session()->get('language') == 'hindi') {{ $product->product_name_hin }} @else {{ $product->product_name_en }} @endif</a></h3>
+        <div class="rating rateit-small"></div>
+        <div class="description"></div>
+        @if ($product->discount_price == NULL) 
+        <div class="product-price"> <span class="price"> ${{ $product->selling_price }} </span></div>
+        <!-- /.product-price --> 
 
-                            @else
-                            <div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">${{ $product->selling_price }}</span> </div>
-                            <!-- /.product-price --> 
+        @else
+        <div class="product-price"> <span class="price"> ${{ $product->discount_price }} </span> <span class="price-before-discount">${{ $product->selling_price }}</span> </div>
+        <!-- /.product-price --> 
 
-                            @endif
-                             
-                          </div>
-                          <!-- /.product-info -->
-                          <div class="cart clearfix animate-effect">
-                            <div class="action">
-                              <ul class="list-unstyled">
-                                <li class="add-cart-button btn-group">
-                                  <button data-toggle="tooltip" class="btn btn-primary icon" type="button" title="Add Cart"> <i class="fa fa-shopping-cart"></i> </button>
-                                  <button class="btn btn-primary cart-btn" type="button">@if (session()->get('language') == 'hindi') कार्ट में जोड़ें @else ADD TO CART @endif</button>
-                                </li>
-                                <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
-                                <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
-                              </ul>
-                            </div>
-                            <!-- /.action --> 
-                          </div>
-                          <!-- /.cart --> 
-                        </div>
-                        <!-- /.product --> 
-                        
-                      </div>
-                      <!-- /.products --> 
-                    </div>
-                    <!-- /.item -->
-                    @endforeach
+        @endif
+          
+      </div>
+      <!-- /.product-info -->
+      <div class="cart clearfix animate-effect">
+        <div class="action">
+          <ul class="list-unstyled">
+            <li class="add-cart-button btn-group">
 
-                  </div>
-                  <!-- /.home-owl-carousel --> 
-                </div>
-                <!-- /.product-slider --> 
-              </div>
-              <!-- /.tab-pane -->
+              <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal"> <i class="fa fa-shopping-cart"></i> </button>
+              <button class="btn btn-primary cart-btn" type="button">@if (session()->get('language') == 'hindi') कार्ट में जोड़ें @else ADD TO CART @endif</button>
+            </li>
+            <li class="lnk wishlist"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Wishlist"> <i class="icon fa fa-heart"></i> </a> </li>
+            <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
+          </ul>
+        </div>
+        <!-- /.action --> 
+      </div>
+      <!-- /.cart --> 
+    </div>
+    <!-- /.product --> 
+    
+  </div>
+  <!-- /.products --> 
+</div>
+<!-- /.item -->
+@endforeach
+
+</div>
+<!-- /.home-owl-carousel --> 
+</div>
+<!-- /.product-slider --> 
+</div>
+<!-- /.tab-pane -->
 
 
 
