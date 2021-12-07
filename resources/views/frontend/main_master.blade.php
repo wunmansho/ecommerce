564d9a561aa421a76e457d84b72d212eb88dba05
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-12-07 13:05:33 -0500

    (214) Product View Modal With Ajax Part 4

--- resources/views/frontend/main_master.blade.php
+++ resources/views/frontend/main_master.blade.php
@@ -92,11 +92,11 @@ break;
 @endif
 </script>  
 <!-- Add to Cart Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" >
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel"><span id="pname"></span></h5>
+        <h5 class="modal-title" id="exampleModalLabel"><strong><span id="pname"></span></strong></h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -112,11 +112,13 @@ break;
   </div>
   <div class="col-md-4">
     <ul class="list-group">
-      <li class="list-group-item">Product Price: <strong id="price"></strong></li>
+      <li class="list-group-item">Product Price: <strong class="text-danger">$<span  id="pprice"></span></strong>
+      <del id="oldprice"></del></li>
       <li class="list-group-item">Product Code: <strong id="pcode"></strong></li>
       <li class="list-group-item">Category: <strong id="pcategory"></strong></li>
       <li class="list-group-item">Brand: <strong id="pbrand"></strong></li>
-      <li class="list-group-item">Stock: <strong id="pstock"></strong></li>
+      <li class="list-group-item">Stock: <span class="badge badge-pill badge-success" id="available" style="background: green; color: white;">
+      </span><span class="badge badge-pill badge-success" id="stockout" style="background: red; color: white;"></span></li>
     </ul>
   </div>
   <div class="col-md-4">
@@ -168,6 +170,31 @@ function productView(id){
     $('#pcategory').text(data.product.category.category_name_en);
     $('#pbrand').text(data.product.brand.brand_name_en);
     $('#pimage').attr('src','/'+data.product.product_thumbnail);
+    //Product Price
+
+    if (data.product.discount_price == null) {
+      $('#pprice').text('');
+      $('#oldprice').text('');
+       $('#pprice').text(data.product.selling_price);
+       
+    }else{
+      $('#pprice').text('');
+      $('#oldprice').text('');
+      $('#pprice').text(data.product.discount_price);  
+      $('#oldprice').text(data.product.selling_price);
+
+    }
+
+    if (data.product.product_qty > 0 ){
+      $('#available').text('');
+      $('#stockout').text('');
+      $('#available').text('available');
+    }else{
+      $('#available').text('');
+      $('#stockout').text('');
+      $('#stockout').text('stockout');
+    }
+
 
     // Color
     $('select[name="color"]').empty();
@@ -196,6 +223,8 @@ function productView(id){
     
    }
  })
+
+ 
 }
 </script>
 </body>
