1acf3fc96ba24f242f457f9bc63fb8099ac0f4bc
Myron Rogers <wunmansho.wunmansho@gmail.com>
2021-12-21 18:45:50 -0500

    (265) Stripe Payment Gateway ¦ Order Placed Success Part 1

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -4,22 +4,59 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Session;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Carbon;
 class StripeController extends Controller
 {
     public function StripeOrder(Request $request){
+        
+        if (Session::has('coupon')) {
+            $total_amount = Session::get('coupon')['total_amount'];
+        } else {
+            $total_amount = round(Cart::total());
+        }
+        
  
 \Stripe\Stripe::setApiKey('sk_test_51K9DNNHKzSPTGjObvzmJf6AYNUVMoJm4MAlOhfkiqvHSuqJd4426em8Z1gGWih4m8y9hy2tvcsIMy1eZm7cM7NQ100vwhHEx7z');
 
 $token = $_POST['stripeToken'];
 
 $charge = \Stripe\Charge::create([
-  'amount' => 999,
+  'amount' => $total_amount*100,
   'currency' => 'usd',
   'description' => 'My Online Store',
   'source' => $token,
-  'metadata' => ['order_id' => '6735'],
+  'metadata' => ['order_id' => uniqid()],
 ]);
-        dd($charge);
+     //   dd($charge);
+
+     $order_id = Order::insertGetId([
+         'user_id' => Auth::id(),
+         'division_id' => $request->division_id,
+         'district_id' => $request->district_id,
+         'state_id' => $request->state_id,
+         'name' => $request->name,
+         'email' => $request->email,
+         'phone' => $request->phone,
+         'postal_code' => $request->postal_code,
+         'notes' => $request->notes,
+         'payment_type' => 'Stripe',
+         'payment_method' => 'Stripe',
+         'payment_type' => $charge->payment_method,
+         'transaction_id' => $charge->balance_transaction,
+         'currency' => $charge->currency,
+         'amount' => $total_amount,
+         'order_number' => $charge->metadata->order_id,   	
+     	'invoice_no' => 'EOS'.mt_rand(10000000,99999999),
+     	'order_date' => Carbon::now()->format('d F Y'),
+     	'order_month' => Carbon::now()->format('F'),
+     	'order_year' => Carbon::now()->format('Y'),
+     	'status' => 'Pending',
+     	'created_at' => Carbon::now(),	 
+     ]);
     }
 }
